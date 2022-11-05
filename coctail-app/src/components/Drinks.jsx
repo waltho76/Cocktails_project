b7d21dd4ccdf545e779920c6535eb2f7b67dae56
Walter Thomas <walterdthomas@me.com>
2022-11-05 12:19:27 -0400

    Walter Thomas

--- coctail-app/src/components/Drinks.jsx
+++ coctail-app/src/components/Drinks.jsx
@@ -1,20 +1,20 @@
 import { useState, useEffect } from "react"
 import axios from 'axios'
-// import { Link } from "react-router-dom"
+import { Link } from "react-router-dom"
 
 export default function Drinks () {
         
 // we need to create a state for our data
 const [drinks, setDrinks] = useState({})
-//const [selectedDrink, setSelectedDrink] = useState(null)
+const [selectedDrink, setSelectedDrink] = useState(null)
 //we need to call an axios function
 
 useEffect(()=> {
     const getData = async () => {
         const response = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=')
-        
-            console.log(response.data)
-            setDrinks(response.data)           
+            console.log(response.data.drinks)
+            setDrinks(response.data.drinks)    
+            setSelectedDrink(true)
     }
     getData()
 }, [])
@@ -29,19 +29,27 @@ if (!drinks) {
     return (
         
         <div className="drinks">
+            {drinks?.map((drink)=> (
+            
+                <div key={drink.idDrink}>
+                    <h2>{drink.strDrink}</h2>
+                    <img src={drink.strDrinkThumb} alt="#"/>
+                    <p>{drink.strIngredient1}{drink.strMeasure1}</p>
+                    <p>{drink.strInstructions}</p>
+                </div>
+            ))}
+
             {/* <div className="imgbox">
                 <img src={image} alt={name}/>
             </div>
             <div className="drink-info">
                 <h3>{strDrink}</h3>
-                <p>{strIngredient}{strMeasure}</p>
-                <p>{strInstructions}</p>
+               
                 <Link to={strVideo} className="video">
                     Video
                 </Link>
 
             </div> */}
-           
             <h2> Drinks Working</h2>
             
         </div>  
@@ -50,4 +58,3 @@ if (!drinks) {
 )}
 }
 
-// //we need to see the data
